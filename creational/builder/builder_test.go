7697bff87f784d2f7c85e22967ed84403a499b50
Yatender Singh <yatender.nitk@gmail.com>
2020-06-13 07:28:48 +0530

    adding builder creational dummy files

---
+++ creational/builder/builder_test.go
@@ -0,0 +1,10 @@
+package creational
+
+// Acceptance criteria
+// Must have a manufacturing type
+
+// VechicleProduct with four wheels, five seats,and a structure defined as car must be returned.
+
+// VechicleProduct with two wheels, two seats and a structure defined as motorbike must be returned.
+
+// VechicleProduct built by any builderProcess builder must be open to modifications.
